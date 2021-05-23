0927425f2916246ddf3d9b0a5460f7b622bf89fc
igor <sferreira.igor@gmail.com>
2021-05-22 22:31:51 -0300

    primeiro envio

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+
+
+start_number = 1
+end_number = int(input("Find prime numbers up to: "))
+
+start = time.time()
+primes = []
+no_primes = 0
+
+print("Looking for prime numbers up to " + str(end_number))
+
+for candidate_number in range(start_number, end_number, 1):
+    found_prime = True
+    for div_number in range(2, candidate_number):
+        if candidate_number % div_number == 0:
+            found_prime = False
+            break
+    if found_prime:
+        primes.append(candidate_number)
+        no_primes += 1
+
+end = round(time.time() - start, 2)
+
+print("Elapsed time: " + str(end) + " seconds")
+print("Prime numbers found: " + str(no_primes))
+print(primes)
